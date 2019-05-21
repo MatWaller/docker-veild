9f8d4db1fe9ef21909046e0017a649ce5bb91e1c
Mathew Waller <wallermadev@gmail.com>
2019-05-21 04:55:26 +0100

    adding key update

--- bootstrap-host.sh
+++ bootstrap-host.sh
@@ -23,6 +23,8 @@ fi
 
 free -m
 
+sudo apt-key update
+
 if [ "$distro" = "trusty" -o "$distro" = "ubuntu:14.04" ]; then
     echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 
